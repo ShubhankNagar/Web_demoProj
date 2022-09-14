25e236765db006623d36bd9b36582da775504895
ShubhankNagar <100033205+ShubhankNagar@users.noreply.github.com>
2022-09-14 18:51:09 +0530

    Add files via upload

---
+++ js/resp.js
@@ -0,0 +1,7 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+rightNav = document.querySelector('.rightNav')
+
+burger.addEventListener('click', ()=>{
+    
+})
